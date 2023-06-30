9387eb27f0f58eed4964303605e3967b40edd111
afgolik <64197556+afgolik@users.noreply.github.com>
2023-06-30 22:27:20 +0300

    создал контекстное меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 import './styles.css'
+import {ContextMenu} from "@/menu";
 
+const contextMenu = new ContextMenu('#menu')
+
+document.body.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    contextMenu.open(e)
+})
+// contextMenu.add(moduleName) - добавление элемента в контекстное меню
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,24 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
-
+    constructor(selector) {
+        super(selector)
+    }
+    open(e) {
+        this.el.classList.add('open')
+        const windowWidth = window.innerWidth
+        const menuWidth = 150
+        this.el.style.top = e.clientY + 'px'
+        if (windowWidth <= e.clientX + menuWidth){
+            this.el.style.left = e.clientX - menuWidth + 'px'
+        } else {
+            this.el.style.left = e.clientX + 'px'
+        }
+    }
+    close() {
+        this.el.classList.remove('open')
+    }
+    add(module) {
+        this.el.insertAdjacentHTML('beforeend', module.toHTML())
+    }
 }
