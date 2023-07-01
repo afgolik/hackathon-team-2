eb804b566109a0a50ac804995261c5d6be32482d
Brother Gadyukin <b.gadyukin@yandex.ru>
2023-07-01 11:10:12 +0300

    crerate background module

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './styles.css'
 import {ContextMenu} from "@/menu";
+import { BackgroundModule } from './modules/background.module';
 
 const contextMenu = new ContextMenu('#menu')
 
 document.body.addEventListener('contextmenu', (e) => {
     e.preventDefault()
     contextMenu.open(e)
+    contextMenu.add(BackgroundModule)
 })
 // contextMenu.add(moduleName) - добавление элемента в контекстное меню
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,22 @@
 import {Module} from '../core/module'
+import { random } from '../utils'
 
 export class BackgroundModule extends Module {
+    #random
+
+    constructor() {
+        this.#random = random
+    }
+
+    trigger() {
+        r = random(0, 255)
+        g = random(0, 255)
+        b = random(0, 255)
+        color = `rgb(${r}, ${g}, ${b})`
+    }
+
+    toHTML() {
+        document.body.style.backgroundColor = color
+    }
 
 }
