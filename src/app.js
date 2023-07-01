e307f0798f8163de92a37ea49377c60a22fbdb05
afgolik <64197556+afgolik@users.noreply.github.com>
2023-07-01 23:51:02 +0300

    реализовал модуль изменения курсора и возврата на стандартный

--- src/app.js
+++ src/app.js
@@ -16,12 +16,13 @@ menu.addEventListener('click', (e) => {
     modules[e.target.dataset.type].trigger()
     contextMenu.close()
 })
-function createModule(moduleClass, type, text){
-    const module = new moduleClass(type, text)
+function createModule(moduleClass, type, text, ...params){
+    const module = new moduleClass(type, text, ...params)
     modules[type] = module
     contextMenu.add(module)
 }
 
 createModule(BackgroundModule, 'backgroundColor', 'Случайный фон')
 createModule(ClicksModule, 'clicksModule', 'Аналитика кликов')
-createModule(CursorModule, 'cursorModule', 'Сменить курсор')
+createModule(CursorModule, 'cursorModule', 'Сменить курсор', ['help', 'pointer', 'progress', 'wait', 'cell', 'no-drop', 'grab'])
+createModule(CursorModule, 'defaultCursorModule', 'Вернуть курсор', ['default'])
--- src/modules/cursor.module.js
+++ src/modules/cursor.module.js
@@ -2,9 +2,9 @@ import {Module} from "@/core/module"
 import {random} from "@/utils";
 
 export class CursorModule extends Module{
-    constructor(type, text) {
+    constructor(type, text, cursors) {
         super(type, text);
-        this.cursors = ['help', 'pointer', 'progress', 'wait', 'cell', 'no-drop', 'grab']
+        this.cursors = cursors
     }
     trigger() {
         document.body.style.cursor = this.cursors[random(0, this.cursors.length - 1)]
